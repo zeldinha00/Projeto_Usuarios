95b6d8f29430cfc6d47492c1e1da6cd7c524fc66
Roger Oliveira Silva <zeldinhamtx@gmail.com>
2022-03-29 10:28:37 -0300

    :up: atualização

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-var name = document.querySelector('#exampleInputName');
-var gender = document.querySelectorAll('#form-user-create [name=gender]:checked');
-var birth = document.querySelector('#exampleInputBirth');
-var country = document.querySelector('#exampleInputCountry');
-var email = document.querySelector('#exampleInputEmail');
-var password = document.querySelector('#exampleInputPassword');
-var photo = document.querySelector('#exampleInputFile');
-var admin = document.querySelector('#exampleInputAdmin');
+var fields = document.querySelectorAll('#form-user-create [name]');
+var user = {};
 
+document.getElementById('form-user-create').addEventListener('submit', function(event){
+
+    fields.forEach(function(field, index){
+
+        event.preventDefault();
+
+        if(field.name == 'gender'){
+    
+            if(field.checked){
+                user[field.name] = field.value;
+            }
+    
+        }else {
+            user[field.name] = field.value;
+        }
+    });
+    console.log(user);
+})
