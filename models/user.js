f09c707ed12d18dbfb89d897d61a751d72552705
Roger Oliveira Silva <zeldinhamtx@gmail.com>
2022-04-01 11:27:46 -0300

    :package: Nova funcionalidade

--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@ class User {
 
     constructor(name, gender, birth, country, email, password, photo, admin){
 
+        this._id;
         this._name = name;
         this._gender = gender;
         this._birth = birth;
@@ -14,6 +15,10 @@ class User {
 
     }
 
+    get id(){
+        return this._id;
+    }
+
     get register(){
         return this._register;
     }
@@ -21,30 +26,132 @@ class User {
     get name(){
         return this._name;
     }
-    get gender(){
+
+    get gender() {
         return this._gender;
     }
-    get birth(){
+
+    get birth() {
         return this._birth;
     }
-    get country(){
+
+    get country() {
         return this._country;
     }
-    get email(){
+
+    get email() {
         return this._email;
     }
-    get password(){
-        return this._password;
-    }
-    get photo(){
+
+    get photo() {
         return this._photo;
     }
-    get admin(){
+
+    get password() {
+        return this._password;
+    }
+
+    get admin() {
         return this._admin;
     }
 
     set photo(value){
         this._photo = value;
+    }
+
+    loadFromJSON(json){
+
+        for (let name in json){
+            
+            switch(name){
+
+                case '_register':
+                    this[name] = new Date(json[name]);
+                break;
+                default:
+                    this[name] = json[name];
+
+            }
+            
+
+        }
+
+    }
+
+    static getUsersStorage() {
+
+        let users = [];
+
+        if (localStorage.getItem("users")) {
+
+            users = JSON.parse(localStorage.getItem("users"));
+
+        }
+
+        return users;
+
+    }
+
+    getNewID(){
+
+        let usersID = parseInt(localStorage.getItem("usersID"));
+
+        if (!usersID > 0) usersID = 0;
+
+        usersID++;
+
+        localStorage.setItem("usersID", usersID);
+
+        return usersID;
+
+    }
+
+    save(){
+
+        let users = User.getUsersStorage();
+
+        if (this.id > 0) {
+            
+            users.map(u=>{
+
+                if (u._id == this.id) {
+
+                    Object.assign(u, this);
+
+                }
+
+                return u;
+
+            });
+
+        } else {
+
+            this._id = this.getNewID();
+
+            users.push(this);
+
+        }
+
+        localStorage.setItem("users", JSON.stringify(users));
+
+    }
+
+    remove(){
+
+        let users = User.getUsersStorage();
+
+        users.forEach((userData, index)=>{
+
+            if (this._id == userData._id) {
+
+                users.splice(index, 1);
+
+            }
+
+        });
+
+        localStorage.setItem("users", JSON.stringify(users));
+
     }
 
 }
